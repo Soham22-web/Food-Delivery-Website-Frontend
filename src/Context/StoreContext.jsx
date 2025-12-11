ad8a7fa442678518e30c9b370d7267b13e6cfdaf
Soham <sohamchavan2211@gmail.com>
2025-12-11 18:01:18 +0530

    env changes done

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+     const url = "http://localhost:4000";
 
 //   const url = "https://food-delivery-website-backend-t378.onrender.com"
 
